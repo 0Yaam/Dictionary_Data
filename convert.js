2ab2c4c903bce70a33dc9859c385559c10e31263
0Yaam <nguyenngoctruongdan@gmail.com>
2024-12-29 13:45:04 +0700

    Update convert.js

--- convert.js
+++ convert.js
@@ -7,12 +7,12 @@ const rawData = fs.readFileSync("dictionary.txt", "utf-8");
 const words = rawData.split("@").slice(1).map((entry) => {
   const lines = entry.trim().split("\n");
   const wordLine = lines[0].split(" ");
-  const word = wordLine[0]; // Lấy từ
-  const ipa = wordLine[1] || ""; // Lấy phiên âm
-  const definition = lines.slice(1).join("\n"); // Lấy nghĩa
+  const word = wordLine[0]; 
+  const ipa = wordLine[1] || ""; 
+  const definition = lines.slice(1).join("\n"); 
   return { word, ipa, definition };
 });
 
 // write
 fs.writeFileSync("data.json", JSON.stringify(words, null, 2));
-console.log("Chuyển đổi hoàn tất! File được lưu dưới tên data.json");
+console.log("Done! File được lưu dưới tên data.json");
